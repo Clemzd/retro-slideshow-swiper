cf159cfd2bb34dedc81488d8c450934521df3746
c.guet <c.guet@emotic.fr>
2023-04-21 14:16:05 +0200

    ajustement pour rendu sur iphone

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/slideshow-angular-swiper'),
+      dir: require('path').join(__dirname, './coverage/retro-slideshow-swiper'),
       subdir: '.',
       reporters: [
         { type: 'html' },
